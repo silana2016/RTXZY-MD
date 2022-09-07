00b52047b432511cee973260e7473ae49572de3a
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-07 15:57:47 +0200

    Update _flaming2.js

--- plugins/_flaming2.js
+++ plugins/_flaming2.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'hadlcomande ila bghiti tsayb logo ex: .flaming2 noureddine_ouafy'
   m.reply('_tsna chwia akhoya osmhlina "afak ila t3tlna alik..._')
   let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'gura.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'gura.jpg', `© instagram.com/noureddine_ouafy`, m, false)
 }
 handler.help = ['flaming2'].map(v => v + ' <>')
 handler.tags = ['maker']
