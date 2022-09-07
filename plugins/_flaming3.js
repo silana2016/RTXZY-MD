ba44e481aa29f2513ba6d1a3bb1ec86d0a2fe183
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-07 16:16:58 +0200

    Update _flaming3.js

--- plugins/_flaming3.js
+++ plugins/_flaming3.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming3 BOTCAHX'
-  m.reply('_Proses..._')
+  if (!args[0]) throw 'had lcomande ila bghiti tsayb logo ex: *.flaming2 noureddine_ouafy*'
+  m.reply('_tsna chwia akhoya osmhlina "afak ila t3tlna alik..._')
   let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'gura.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'gura.jpg', `© instagram.com/noureddine_ouafy`, m, false)
 }
 handler.help = ['flaming3'].map(v => v + ' <text>')
 handler.tags = ['maker']
