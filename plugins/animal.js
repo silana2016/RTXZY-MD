3a81b081f597b4c3f5a8f8f0e57e6657bc5e8254
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-07 18:06:14 +0200

    Update animal.js

--- plugins/animal.js
+++ plugins/animal.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 ┌
 ${ar.map(v => '├ ' + v).join`\n`}
 └────
-Contoh:
+*matalan dir hakka*:
 ${usedPrefix}${command} panda
 `.trim()
   if (!text) throw er
