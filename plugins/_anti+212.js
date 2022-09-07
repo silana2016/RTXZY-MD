eace8aee59791d55ed7d49cc2699097338de2d7f
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-07 19:15:47 +0200

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,7 +1,7 @@
 let handler = m => m
 
 handler.before = async function (m) {
-   if (m.sender.startsWith('62' || '212')) {
+   if (m.sender.startsWith('62' || '212' || '94')) {
    	global.db.data.users[m.sender].banned = true
    }
    
