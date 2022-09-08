da9a8d4bc81ca8c880b0bedf0273ddbcf6e75756
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-08 19:02:56 +0200

    Update _restmaker2.js

--- plugins/_restmaker2.js
+++ plugins/_restmaker2.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming4 BOTCAHX'
-  m.reply('_Proses..._')
+  if (!args[0]) throw 'had lcomande ila bghiti tsayb logo ex: .logo4 noureddine_ouafy'
+  m.reply('_tsna chwia akhoya osmhlina "afak ila t3tlna alik..._')
   let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/shadow-sky?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© instagram.com/noureddine_ouafy`, m, false)
 }
 handler.help = ['flaming8'].map(v => v + ' <text>')
 handler.tags = ['maker']
-handler.command = /^(flaming8)$/i
+handler.command = /^(logo4)$/i
 
 module.exports = handler
