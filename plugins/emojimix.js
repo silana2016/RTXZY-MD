073a91f5ca91fa35ee56804cdf8953118420a5ea
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-08 17:58:34 +0200

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -47,7 +47,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 
 let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw 'hada imkn lik tjm3 joj dyal emojie f emojie wahd  dir bhal haka   penggunaan:\n\n*.emojimix 🤨+😣*'
+if (!args[0]) throw 'hada imkn lik tjm3 joj dyal emojie f emojie wahd  dir bhal haka     :\n\n\n\n*.emojimix 🤨+😣*'
    	let [emoji1, emoji2] = text.split`+`
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 		for (let res of anu.results) {
