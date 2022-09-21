5913e907b316c40d3964ff2086b6338b094ab5a9
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-21 09:16:21 +0100

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,6 +1,7 @@
 const { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `هاد الامر الا بغيتي تيليشارجي شي فيديو من فيسبوك دير مثلا بحال هاكا  ${usedPrefix}${command} https://www.facebook.com/100063533185520/posts/510616167732818/`
+    if (!args[0]) throw `هاد الامر الا بغيتي تيليشارجي شي فيديو من فيسبوك دير مثلا بحال هاكا \n
+ ${usedPrefix}${command} https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`
     const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
     for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `© https://instagram.com/noureddine_ouafy`, m)
 }
