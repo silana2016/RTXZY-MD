f09fcb5744cf8c0e49728cf8bace26e62fafd776
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-09-22 16:46:01 +0100

    Update _flaming1.js

--- plugins/_flaming1.js
+++ plugins/_flaming1.js
@@ -2,10 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join('hadlcomande ila bghiti tsayb logo ex: .logo1 noureddine_ouafy ').split('|')
   if (!args[0]) throw ''
-  m.reply('__─────────────────────
-*tsna chwia lah yhfdak 🫡*
-─────────────────────
-```☘︎ вσвιz вσт ☘︎```..._')
+  m.reply('tsna xwia')
   let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&script=fluffy-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
   conn.sendFile(m.chat, res, 'gura.jpg', `© instagram.com/noureddine_ouafy `, m, false)
 }
